0ab94cb19cb5a9265a9e3971ddb31a204e91e470
dipesh digwal <dipeshdigwal1@icloud.com>
2020-12-13 21:48:08 +0530

    first

---
+++ test.js
@@ -0,0 +1,5 @@
+function tes(){
+    document.getElementById("para1").innerHTML="hello test user!!"
+}
+alert("#table1 tr".length);  //get the number of rows in the table
+document.getElementById("td1").setAttribute("bgcolor","#C0C0C0")
